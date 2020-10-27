a79e5eec084c0fa54b8b72d5025602c148fd9f92
JoseLucasapp <josluc2016@gmail.com>
2020-10-27 19:38:40 -0300

    css update

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 let io = require('socket.io')(http);
-const port = 3000;
+const port = process.env.port || 3000;
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/pages/index.html');
 });
 
 io.on('connection',(socket)=>{
-    console.log('conect');
-    socket.on('disconnect',()=>{
-        console.log('disconect');
-    });
     socket.on('message',(message,name)=>{
         io.emit('message',message,name);
     });
