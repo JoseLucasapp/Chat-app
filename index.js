1d3f5ecdae1f6a814953116ea2f0ae9d1cb28864
JoseLucasapp <josluc2016@gmail.com>
2020-10-27 11:36:23 -0300

    add socket

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+let io = require('socket.io')(http);
 const port = 3000;
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/pages/index.html');
 });
 app.get('/chat', (req,res)=>{
     res.sendFile(__dirname + '/pages/chat.html');
+});
+
+io.on('connection',(socket)=>{
+    socket.on('disconnect',()=>{
+        console.log('disconect');
+    });
+    socket.on('name',(name)=>{
+        console.log(name)
+    })
 });
 
 http.listen(port);
