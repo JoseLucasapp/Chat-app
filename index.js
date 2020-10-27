f845d6ccf6635cc345c940b5b648222ce6ff855b
JoseLucasapp <josluc2016@gmail.com>
2020-10-27 11:23:24 -0300

    update

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const port = 3000;
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/pages/index.html');
 });
+app.get('/chat', (req,res)=>{
+    res.sendFile(__dirname + '/pages/chat.html');
+});
 
 http.listen(port);
