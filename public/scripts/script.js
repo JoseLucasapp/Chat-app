7ec8cea236dec98717c66b059821ddfd165c53ab
JoseLucasapp <josluc2016@gmail.com>
2020-11-10 17:16:31 -0300

    update style

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -18,13 +18,16 @@ document.querySelector('#username').addEventListener('blur',()=>{
     if(name.length > 0){
         document.getElementById('btnuser').disabled = false;
         document.getElementById('sendbtn').disabled = false;
-        document.getElementById('btnuser').style.borderColor = 'blue';
-        document.getElementById('btnuser').style.color = 'blue';
+        document.getElementById('btnuser').style.borderColor = 'green';
+        document.getElementById('btnuser').style.color = 'green';
+        document.querySelector('#username').style.borderBottom = '1px solid green';
+        document.querySelector('#username').style.color = 'green';
     }
     else{
         document.getElementById('btnuser').disabled = true;
         document.getElementById('btnuser').style.borderColor = 'gray';
         document.getElementById('btnuser').style.color = 'gray';
+        document.querySelector('#username').style.borderBottom = '1px solid purple';
     }
 });
 
