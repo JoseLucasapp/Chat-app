aeb1320f6c0f83bfecfe4fc6724716c3c8858ba7
JoseLucasapp <josluc2016@gmail.com>
2020-11-27 19:27:26 -0300

    add server

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/public/index.html');
 });
