8803b6e9c7666c828c3c1991cc4487a47664142e
JoseLucasapp <josluc2016@gmail.com>
2020-12-02 15:20:03 -0300

    add consign

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const consign = require('consign');
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(express.static('./app/public'));
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -4,11 +4,6 @@ let io = require('socket.io')(http);
 let consign = require('consign');
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs');
-});
-
-
 messages = [];
 users = 0;
 
