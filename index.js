d00304e156642adb6b16313d913f01fb17996686
joselucasapp <josluc2016@gmail.com>
2021-01-16 11:06:47 -0300

    add updates

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let application = require('./config/server');
-const http = require('http').createServer(application);
+const express = require('express');
+const app = express();
+const consign = require('consign');
+const http = require('http').createServer(app);
 let io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-application.app.models.index.ioModel(io);
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
 
-http.listen(port);
+app.use(express.static('./app/public'));
+
+consign().include('app/routes')
+    .then('app/models')
+    .into(app);
+
+app.app.models.index.ioModel(io);
+
+http.listen(PORT);
